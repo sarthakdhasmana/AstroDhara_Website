fe85830885e0fd1fdf9539ec24a7af10383a8db2
sarthakdhasmana <sarthakmay09@gmail.com>
2022-07-28 09:07:00 +0530

    first commit all the files of landing page added initially

---
+++ js/ServiceSection.js
@@ -0,0 +1,6 @@
+var exploreBtn = document.querySelector('.knowMorebtn');
+var arrow = document.querySelector('.arrow');
+
+exploreBtn.addEventListener('mouseenter', () => {
+    arrow.classList.add('arrowActive');
+})
