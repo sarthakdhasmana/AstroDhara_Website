713a1499b1da04a813ca8a594f5f4b88a4f211aa
sarthakdhasmana <sarthakmay09@gmail.com>
2022-08-03 09:34:39 +0530

    website desktop version fully done-added new images and made some small changes in hero section

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,5 @@
+
+//exploreServices btn service section
 var exploreBtn = document.querySelector('.knowMorebtn');
 var arrow = document.querySelector('.arrow');
 
@@ -6,9 +8,28 @@ exploreBtn.addEventListener('mouseenter', () => {
 })
 
 
+//contact btn navbar
 var contactbtn = document.querySelector('.contactBtn');
 var locateSection = document.querySelector('.location_section');
 
 contactbtn.addEventListener('click', () => {
     locateSection.scrollIntoView({ behavior: 'smooth' });
+})
+
+
+//connect btn
+var connectBtn = document.querySelector('.connectbtn');
+var contactForm = document.querySelector('.contactForm_section');
+
+connectBtn.addEventListener('click', () => {
+    contactForm.scrollIntoView({ behavior: 'smooth' });
+})
+
+
+//explore btn
+var explore = document.querySelector('.Explorebtn');
+var explorenow = document.querySelector('.image_Slider');
+
+explore.addEventListener('click', () => {
+    explorenow.scrollIntoView({ behavior: 'smooth' });
 })
