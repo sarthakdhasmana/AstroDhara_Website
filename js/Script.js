8f524a4b01b152aa8e26385e7c5961a1db936315
sarthakdhasmana <sarthakmay09@gmail.com>
2022-08-05 10:23:10 +0530

    major changes done in the UI also Mobile View responsive website done

--- js/Script.js
+++ js/Script.js
@@ -1,19 +1,26 @@
 
 //exploreServices btn service section
-var exploreBtn = document.querySelector('.knowMorebtn');
-var arrow = document.querySelector('.arrow');
+var exploreBtn = document.querySelectorAll('.knowMorebtn');
+var arrow = document.querySelectorAll('.arrow');
 
-exploreBtn.addEventListener('mouseenter', () => {
-    arrow.classList.add('arrowActive');
+exploreBtn[0].addEventListener('mouseenter', () => {
+    arrow[0].classList.add('arrowActive');
+})
+exploreBtn[1].addEventListener('mouseenter', () => {
+    arrow[1].classList.add('arrowActive');
 })
 
 
 //contact btn navbar
 var contactbtn = document.querySelector('.contactBtn');
 var locateSection = document.querySelector('.location_section');
+var mobileNav = document.querySelector('.mobileNav');
+var Ham = document.querySelector('.hamburger ');
 
 contactbtn.addEventListener('click', () => {
     locateSection.scrollIntoView({ behavior: 'smooth' });
+    mobileNav.classList.remove('is-active');
+    Ham.classList.remove('is-active');
 })
 
 
@@ -32,4 +39,12 @@ var explorenow = document.querySelector('.image_Slider');
 
 explore.addEventListener('click', () => {
     explorenow.scrollIntoView({ behavior: 'smooth' });
+})
+
+//hamburger menu
+var hamburger = document.querySelector('.hamburger');
+var navArea = document.querySelector('.mobileNav');
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
+    navArea.classList.toggle('is-active');
 })
--- public/js/Script.js
+++ public/js/Script.js
@@ -1,14 +1,50 @@
-var exploreBtn = document.querySelector('.knowMorebtn');
-var arrow = document.querySelector('.arrow');
 
-exploreBtn.addEventListener('mouseenter', () => {
-    arrow.classList.add('arrowActive');
+//exploreServices btn service section
+var exploreBtn = document.querySelectorAll('.knowMorebtn');
+var arrow = document.querySelectorAll('.arrow');
+
+exploreBtn[0].addEventListener('mouseenter', () => {
+    arrow[0].classList.add('arrowActive');
+})
+exploreBtn[1].addEventListener('mouseenter', () => {
+    arrow[1].classList.add('arrowActive');
 })
 
 
+//contact btn navbar
 var contactbtn = document.querySelector('.contactBtn');
 var locateSection = document.querySelector('.location_section');
+var mobileNav = document.querySelector('.mobileNav');
+var Ham = document.querySelector('.hamburger ');
 
 contactbtn.addEventListener('click', () => {
     locateSection.scrollIntoView({ behavior: 'smooth' });
+    mobileNav.classList.remove('is-active');
+    Ham.classList.remove('is-active');
+})
+
+
+//connect btn
+var connectBtn = document.querySelector('.connectbtn');
+var contactForm = document.querySelector('.contactForm_section');
+
+connectBtn.addEventListener('click', () => {
+    contactForm.scrollIntoView({ behavior: 'smooth' });
+})
+
+
+//explore btn
+var explore = document.querySelector('.Explorebtn');
+var explorenow = document.querySelector('.image_Slider');
+
+explore.addEventListener('click', () => {
+    explorenow.scrollIntoView({ behavior: 'smooth' });
+})
+
+//hamburger menu
+var hamburger = document.querySelector('.hamburger');
+var navArea = document.querySelector('.mobileNav');
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
+    navArea.classList.toggle('is-active');
 })
